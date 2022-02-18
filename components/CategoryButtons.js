337fd62821323501a6c0024ae6e77a1942163f5d
Evolution2Art <luc@2enhance.co>
2022-02-18 17:49:36 +0000

    add isnull detection in CategoryButtons

--- components/CategoryButtons.js
+++ components/CategoryButtons.js
@@ -7,7 +7,7 @@ const CategoryButtons = ({ categories = [] }) => {
       {categories.map((_category) => (
         <Link href={`/categories/${_category.slug}`} key={_category.id}>
           <a className="hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 rounded">
-            {_category.icon.url && <div className="rounded-t-lg pt-2 pb-2 mx-auto w-16 h-16">
+            {_category.icon?.url && <div className="rounded-t-lg pt-2 pb-2 mx-auto w-16 h-16">
               <NextImage media={_category.icon} />
             </div>}
             <h4 className="mt-8">
