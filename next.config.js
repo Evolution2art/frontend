569394e46c341e6eb0e218126284e0d8dff9c460
Evolution2Art <luc@2enhance.co>
2022-03-31 15:58:42 +0000

    Remove all CSP headers

--- next.config.js
+++ next.config.js
@@ -1,19 +1,2 @@
 // next.config.js
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value:
-      "object-src 'self' *.evolution2art.com; script-src 'self' *.evolution2art.com; font-src 'self' fonts.google.com; frame-ancestors 'self' *.evolution2art.com;",
-  },
-]
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: securityHeaders,
-      },
-    ]
-  },
-}
+module.exports = {}
