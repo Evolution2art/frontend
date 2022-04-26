159db3d34e6d76ed65d83c48beaf0efb8bfd18da
Evolution2Art <luc@2enhance.co>
2022-04-26 12:26:27 +0000

    Fix typo

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     return res.status(401).json({ message: "Invalid token" })
   }
 
-  const path = req.qeury.path
+  const path = req.query.path
   try {
     console.log("Calling unstable revalidate for path", path)
     await res.unstable_revalidate(`/${path}`)
